65e246121a426bdc4067f867f7e9a4a4b6c06784
Javier Lopez C <javlc.dev@gmail.com>
2023-08-29 16:37:33 -0400

    Implement Apollo Graphql, implement read query and mutation.

--- client/src/pages/Pets.js
+++ client/src/pages/Pets.js
@@ -5,13 +5,57 @@ import PetsList from '../components/PetsList'
 import NewPetModal from '../components/NewPetModal'
 import Loader from '../components/Loader'
 
+const GET_PETS = gql`
+  query AllPets {
+    pets {
+      id
+      name
+      type
+      img
+    }
+  }
+`
+const ADD_PET = gql`
+  mutation AddPet($newPet: NewPetInput!) {
+    addPet(input: $newPet) {
+      id
+      name
+      type
+      img
+    }
+  }
+`
+
 
 export default function Pets () {
   const [modal, setModal] = useState(false)
 
+  const {data, loading, error} = useQuery(GET_PETS)
+  const [addPet, newPet] = useMutation(ADD_PET, {
+    update(cache, {data: {addPet}}) {
+      const data = cache.readQuery({
+        query: GET_PETS
+      })
+      cache.writeQuery({
+        query: GET_PETS,
+        data: { pets: [addPet, ...data.pets] },
+      })
+    }
+  })
 
   const onSubmit = input => {
     setModal(false)
+    addPet({ 
+      variables: { newPet: input }
+    })
+  }
+
+  if (loading || newPet.loading) {
+    return <Loader />
+  }
+
+  if (error || newPet.error) {
+    return <p>Error</p>
   }
   
   if (modal) {
@@ -32,7 +76,7 @@ export default function Pets () {
         </div>
       </section>
       <section>
-        <PetsList />
+        <PetsList pets={data.pets}/>
       </section>
     </div>
   )
