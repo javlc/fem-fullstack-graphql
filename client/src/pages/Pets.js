42a7d9325b953dd70233e1c677da8b7ad39e5279
Javier Lopez C <javlc.dev@gmail.com>
2023-08-30 18:47:27 -0400

    Implement client-side query extension, implement fragment, type defs, resolvers, apollo link, optimistic response.

--- client/src/pages/Pets.js
+++ client/src/pages/Pets.js
@@ -5,25 +5,35 @@ import PetsList from '../components/PetsList'
 import NewPetModal from '../components/NewPetModal'
 import Loader from '../components/Loader'
 
+const PETS_FIELDS = gql`
+  fragment PetsFields on Pet {
+    id
+    name
+    type
+    img
+    vaccinated @client
+    owner {
+      id
+      age @client
+    }
+  }
+`
+
 const GET_PETS = gql`
   query AllPets {
     pets {
-      id
-      name
-      type
-      img
+      ...PetsFields
     }
   }
+  ${PETS_FIELDS}
 `
 const ADD_PET = gql`
   mutation AddPet($newPet: NewPetInput!) {
     addPet(input: $newPet) {
-      id
-      name
-      type
-      img
+      ...PetsFields
     }
   }
+  ${PETS_FIELDS}
 `
 
 
@@ -40,24 +50,39 @@ export default function Pets () {
         query: GET_PETS,
         data: { pets: [addPet, ...data.pets] },
       })
-    }
+    },
   })
 
   const onSubmit = input => {
     setModal(false)
     addPet({ 
-      variables: { newPet: input }
+      variables: { newPet: input },
+      optimisticResponse: {
+        __typename: 'Mutation',
+        addPet: {
+          __typename: 'Pet',
+          id: Math.floor(Math.random() * 10000) + '',
+          name: input.name,
+          type: input.type,
+          img: 'https://placehold.co/300',
+          owner: {
+            id: Math.floor(Math.random() * 10000) + '',
+            age: 47
+          }
+    
+        }
+      }
     })
   }
 
-  if (loading || newPet.loading) {
+  if (loading) {
     return <Loader />
   }
 
   if (error || newPet.error) {
     return <p>Error</p>
   }
-  
+
   if (modal) {
     return <NewPetModal onSubmit={onSubmit} onCancel={() => setModal(false)} />
   }
